3d31f452051b002fb16ae7459650154b1d50a513
Баяндин Иван <bajandin2015@mail.ru>
2022-05-16 13:44:20 +0300

    16.05.2022

--- Сервер/client/src/App.js
+++ Сервер/client/src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 
 function App() {
-    return ( <
-        div >
-        WORKING <
-        /div>
+    return (
+         <div>
+        WORKING 
+        </div>
     );
 }
 
