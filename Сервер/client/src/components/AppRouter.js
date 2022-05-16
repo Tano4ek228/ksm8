5bc3e1df0d7a27e66b2c97f520a1f1c10fcba298
Баяндин Иван <bajandin2015@mail.ru>
2022-05-16 18:37:15 +0300

    16.05.2022

---
+++ Сервер/client/src/components/AppRouter.js
@@ -0,0 +1,21 @@
+import React from "react";
+import {Routes, Route} from 'react-router-dom'
+import { publicRoutes } from "../routes";
+
+const AppRouter = () => {
+    return (
+        <Routes>
+             {publicRoutes.map(({path, Component})=>
+                <Route path={path} element={<Component/>}/>
+            )}
+            <Route 
+            path="*"
+            element={
+                <p>Нет совпадения</p>
+            }
+            />
+        </Routes>
+    )  
+}
+
+export default AppRouter;
