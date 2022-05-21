eec15dad16e20338da26058e53972961efae1c9a
Баяндин Иван <bajandin2015@mail.ru>
2022-05-21 18:49:00 +0300

    21.05.22

--- Сервер/client/src/http/userAPI.js
+++ Сервер/client/src/http/userAPI.js
@@ -1,14 +1,15 @@
 import { $host } from "./index";
+import jwt_decode from "jwt-decode"
 
-export const registration = (email,password)=>
+export const registration = async (email,password)=>
 {
-    const responce =  $host.post('api/auth/registration',{email,password,role:'ADMIN'})
-    return responce
+    const responce =  $host.post('api/user/registration',{email,password,role:'ADMIN'})
+    return jwt_decode((await responce).data.token)
 }
-export const login = (email,password)=>
+export const login = async (email,password)=>
 {
-    const responce = $host.post('api/auth/login',{email,password})
-    return responce
+    const responce = $host.post('api/user/login',{email,password})
+    return jwt_decode((await responce).data.token)
 }
 export const check = ()=>
 {
