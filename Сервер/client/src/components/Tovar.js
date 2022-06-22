8e412d926a2ba0f670b5e453b6a1a99925e3d6cf
Гапонов Максим <100971620+POX1D20@users.noreply.github.com>
2022-06-23 01:00:51 +0300

    site 23.06.22

--- Сервер/client/src/components/Tovar.js
+++ Сервер/client/src/components/Tovar.js
@@ -1,20 +1,26 @@
 import React, { useContext } from "react";
 import { Context } from "..";
+import '../pages/style/tovar_price.css'
 
 const Tovar = () => {
     const { device } = useContext(Context)
 	return (
-		<div >
-            <table>
+		<div>
+			<div className="tovar_name_line">
+				<h2>Тип</h2>
+				<h2>Цена</h2>
+				<h2>НДС</h2>
+			</div>
+            <table className="table_tovar">
             {device.TypeDevice.map(type => 		
-                <tr>
-                    <tb>
+                <tr className="tovar_tr">
+                    <tb className="tovar_tb tb_right_botder">
                     {type.name}
                     </tb>
-                    <tb>
+                    <tb className="tovar_tb tb_right_botder">
                     {type.price}
                     </tb>
-                    <tb>
+                    <tb className="tovar_tb">
                     {type.NDS}
                     </tb>
                 </tr>
